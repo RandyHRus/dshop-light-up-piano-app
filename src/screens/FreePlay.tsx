3b2d957588f458e971d7ab02a7184b020e1ef30d
Randy Russell <randyhr2000@gmail.com>
2022-03-31 22:15:55 -0700

    Freeplay screen

--- src/screens/FreePlay.tsx
+++ src/screens/FreePlay.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet, Image, ImageBackground } from "react-native";
 import Card from "../components/Card";
 import Screen from "../components/Screen";
@@ -6,6 +6,48 @@ import Text from "../components/Text";
 
 const gradientImage = require("../../assets/gradientBackground.jpg");
 
-const FreePlay = () => <Screen backgroundImage={gradientImage}></Screen>;
+function FreePlay(props: any) {
+    const [displayedNote, setDisplayedNote] = useState("D");
+
+    return (
+        <Screen backgroundImage={gradientImage}>
+            <View style={styles.container}>
+                <Text style={styles.modeText}>Freeplay Mode</Text>
+                <View style={styles.noteContainer}>
+                    <Text style={styles.displayedNote}>{displayedNote}</Text>
+                </View>
+            </View>
+        </Screen>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: "column",
+        display: "flex",
+        flex: 1,
+        padding: 2,
+        alignItems: "center",
+    },
+    noteContainer: {
+        flexDirection: "column",
+        display: "flex",
+        flex: 1,
+        padding: 2,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    displayedNote: {
+        fontFamily: "Nunito",
+        fontSize: 200,
+        fontWeight: "bold",
+    },
+    modeText: {
+        top: 50,
+        fontFamily: "Nunito",
+        fontSize: 24,
+        fontWeight: "bold",
+    },
+});
 
 export default FreePlay;
